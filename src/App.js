22b432861041ebf3866c1ae382048b69fa59cddd
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-11-22 16:32:58 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+import ExperiencePage from "./Components/UserExperience/ExperiencePage";
+
+function App() {
+  return <div>
+    {/* <DonorForm /> */}
+    <ExperiencePage />
+  </div>
+}
+
+export default App;
