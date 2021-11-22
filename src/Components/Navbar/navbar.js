b185c4771f2f1bf73909ae39df70d38703416822
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-11-23 00:49:53 +0530

    Second Commit

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -3,7 +3,22 @@ import styles from './navbar.module.css';
 
 const Navbar = props => {
     return <nav className={styles.navbar}>
-
+        <div className={styles.logo} >
+            <span>Responsive</span>
+        </div>
+        <div className={styles.button}>
+            <i class='bx bx-exit' ></i>
+        </div>
+        <ul className={styles.list}>
+            <li><a href='#' >Home</a></li>
+            <li><a href='#' >About</a></li>
+            <li><a href='#' >Services</a></li>
+            <li><a href='#' >Pricing</a></li>
+            <li><a href='#' >Contact</a></li>
+        </ul>
+        <div className={styles.button}>
+            <i class='bx bx-menu'></i>
+        </div>
     </nav>
 }
 
