f58532b002152bfef626a2ec6488657b0a0fb3dd
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-11-23 17:42:16 +0530

    Finished

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -2,34 +2,38 @@ import React, { useState } from "react";
 import styles from './navbar.module.css';
 
 const Navbar = props => {
-    const [isScrolled, setIsScrolled] = useState(false)
-    const navbarScrollHandler = () => {
-        console.log('scrolled')
-        if (window.scrollY > 0) {
-            setIsScrolled((prevState) => {
-                return !prevState
-            })
-        }
+    const [toggle, setToggle] = useState(false);
+
+    const toggleButtonHandler = () => {
+        setToggle((prevState) => !prevState);
     }
 
-    const navbarClasses = `${styles.navbar} ${isScrolled && styles['sticky-nav']}`;
+    window.addEventListener('scroll', () => {
+        let add = styles['sticky-nav'];
+        let navbar = document.querySelector("nav");
+        navbar.classList.toggle(add, window.scrollY > 10)
+    });
 
-    return <nav className={navbarClasses} onScroll={navbarScrollHandler}>
+    const classes = `${styles.listClose} ${toggle && styles.active}`
+
+    return <nav className={styles.navbar}>
         <div className={styles.logo} >
             <span>Responsive</span>
         </div>
-        <div className={styles.buttonFirst}>
-            <i class='bx bx-exit' ></i>
+        <div className={classes}>
+            <div className={styles.buttonFirst}>
+                <i className='bx bx-exit' onClick={toggleButtonHandler} ></i>
+            </div>
+            <ul className={styles.list}>
+                <li><a href='/' >Home</a></li>
+                <li><a href='/' >About</a></li>
+                <li><a href='/' >Services</a></li>
+                <li><a href='/' >Pricing</a></li>
+                <li><a href='/' >Contact</a></li>
+            </ul>
         </div>
-        <ul className={styles.list}>
-            <li><a href='#' >Home</a></li>
-            <li><a href='#' >About</a></li>
-            <li><a href='#' >Services</a></li>
-            <li><a href='#' >Pricing</a></li>
-            <li><a href='#' >Contact</a></li>
-        </ul>
         <div className={styles.buttonSecond}>
-            <i class='bx bx-menu'></i>
+            <i className='bx bx-menu' onClick={toggleButtonHandler}></i>
         </div>
     </nav>
 }
